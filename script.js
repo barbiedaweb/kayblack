7dff31a2cff33e04c0d32d6609e7c5227b165047
anna <105943198+barbiedaweb@users.noreply.github.com>
2024-08-13 08:02:46 -0300

    ,,,,

--- script.js
+++ script.js
@@ -7,15 +7,40 @@ const textoResultado = document.querySelector(".texto-resultado")
 const perguntas = [
     {
         enunciado: "No âmbito social, você prefere:",
-        alternativas: ["Ter a oportunidade de ganhar uma grande quantidade de dinheiro, mas perder o contato com todos seus amigos, Realizar seu maior sonho de morar em outro país, mas ficar 7 anos sem nenhum contato com sua família e amigos"]
+        alternativas: [
+            {
+                texto: "Ter a oportunidade de ganhar uma grande quantidade de dinheiro, mas perder o contato com todos seus amigos.",
+                afirmacao: "Afirmação 1",
+            },
+            {
+                texto: "Realizar seu maior sonho de morar em outro país, mas ficar 7 anos sem nenhum contato com sua família e amigos.",
+                afirmacao: "Afirmação 2",
+            }]
     },
     {
         enunciado: "No âmbito ambiental, você prefere:",
-        alternativas: ["Ajudar a combater a poluição do oceano com uma nova tecnologia, mas ter uma extinção de animais marinhos, Acabar com o uso de combustíveis fósseis, mas ver metade da população perdendo o emprego"]
-    },
+        alternativas: [
+            {
+                texto: "Ajudar a combater a poluição do oceano com uma nova tecnologia, mas ter uma extinção de animais marinhos",
+                afirmacao: "Afirmação 3",
+            },
+            {
+                texto: "Acabar com o uso d,e combustíveis fósseis, mas ver metade da população perdendo o emprego.",
+                afirmacao: "Afirmação 4"
+            }]
+        },
     {
         enunciado: "No âmbito tecnologico, você prefere:",
-        alternativas: ["Ter uma tecnologia que diz qual a doença do paciente, mas ter metade da população perdendo seu emprego na saúde, Ganhar todo ano o celular do momento, mas ter todas suas informações vazadas"]
+    
+        alternativas: [
+            {
+               texto:"Ter uma tecnologia que diz qual a doença do paciente, mas ter metade da população perdendo seu emprego na saúde.",
+               afirmacao: "Afirmação 5",
+            },
+            {
+                texto: "Ganhar todo ano o celular do momento, mas ter todas suas informações vazadas.",
+                afirmacao: "Afirmação 6"
+            }]
     }
 
 ]
@@ -23,16 +48,16 @@ const perguntas = [
 let atual = 0;
 let perguntaAtual;
 
-function mostraPerguntas (){
+function mostraPerguntas() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
 
 }
 
-function mostraAlternativas (){
+function mostraAlternativas() {
     for (const alternativa of perguntasAtual.alternativas) {
         const botaoAlternativa = document.createElement("button");
-        botaoAlternativa.textContent = alternativa; 
+        botaoAlternativa.textContent = alternativa;
         caixaAlternativas.appendChild(botaoAlternativa);
     }
 }
