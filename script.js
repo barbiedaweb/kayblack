664560469c8aeca59bd9239c2cc690f23ea58e1b
anna <105943198+barbiedaweb@users.noreply.github.com>
2024-08-20 08:02:51 -0300

    bfx

--- script.js
+++ script.js
@@ -62,13 +62,14 @@ function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativa = document.createElement("button");
         botaoAlternativa.textContent = alternativa.texto;
-        botaoAlternativa.addEventListener("click", ()=> opcaoSelecionada(afirmacao));
+        botaoAlternativa.addEventListener("click", ()=> opcaoSelecionada(alternativa);
         caixaAlternativas.appendChild(botaoAlternativa);
     }
 }
 
 function respostaSelecionada (opcaoSelecionada){
     const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal = afirmacoes;
     atual++;
     mostraPerguntas();
 }
